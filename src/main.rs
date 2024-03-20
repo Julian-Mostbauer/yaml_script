115bb925dd506b75337b93952df945b8a3f9eac7
Julian-Mostbauer <146772868+Julian-Mostbauer@users.noreply.github.com>
2024-03-20 20:36:42 +0100

    serialisation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use core::fmt;
+use serde::{Deserialize, Serialize};
+use serde_yaml::{self};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Function {
+    name: String,
+    content: String,
+    return_type: String,
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Variable {
+    name: String,
+    content: String,
+    content_type: String,
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct Code {
+    entry_point: String,
+    variables: Vec<Variable>,
+    functions: Vec<Function>,
+}
+
+impl fmt::Display for Code {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:#?}\n{:#?}", self.variables, self.functions)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = r"C:\Users\julia\OneDrive\Dokumente\GitHub\yaml_script\src\script.yaml";
+    let f = std::fs::File::open(file_path).expect("Could not open file.");
+    let code: Code = serde_yaml::from_reader(f).expect("Could not read values.");
+
+    println!("{}", code.entry_point);
+
+    println!("{}", code);
 }
